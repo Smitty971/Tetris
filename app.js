b44fc895a396da59d8c904620bc10af90d46dd16
Smitty971 <jlsmitty2273@yahoo.com>
2020-11-06 09:40:08 -0500

    added in more movements

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ document.addEventListener('DOMContentLoaded', () => {
         if(e.keyCode === 37) {
             moveLeft()
         }else if (e.keyCode === 38) {
-            //rotate()
+            rotate()
         }else if (e.keyCode === 39) {
             moveRight()
         }else if (e.keyCode === 40) {
@@ -119,4 +119,27 @@ document.addEventListener('DOMContentLoaded', () => {
         draw()
       }
 
+      //move the tetromino right, unless is at the edge or there is a blockage
+      function moveRight() {
+          undraw()
+          const isAtRightEdge = current.some(index => (currentPosition + index) % width === width -1)
+
+          if(!isAtRightEdge) currentPosition +=1
+
+          if(current.some(index => squares[currentPosition + index].classList.contains('taken')))
+          currentPosition -=1
+      }
+
+      draw()
+
+
+//rotate the tetromino
+function rotate() {
+    undraw()
+    currentRotation ++
+    if(currentRotation === current.length) {// if the current roatation gets to 4, make it go back to 0
+    }
+    current = theTetrominoes[random][currentRotation]
+    draw()
+}
 })
