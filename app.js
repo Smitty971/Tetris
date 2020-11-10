fae92e92652364ba89c25bdb20bcf1230a325e5d
Smitty971 <jlsmitty2273@yahoo.com>
2020-11-10 18:58:05 -0500

    added score function

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const width = 10
     let nextRandom = 0
     let timerId
+    let score = 0
 
 
     //The Tetrominoes
@@ -104,6 +105,7 @@ document.addEventListener('DOMContentLoaded', () => {
           currentPosition = 4
           draw()
           displayShape()
+          addScore()
         }
       }
 
@@ -183,6 +185,21 @@ startBtn.addEventListener('click', () => {
     }
 })
 
+//add score
+function addScore() {
+    for (let i = 0; i < 199; i +=width) {
+        const row = [i, i+2, i+3, i+4, i+5, i+6, i+7, i+8, i+9]
+     
+        if(row.every(index => squares[index].classLists.contains('taken'))) {
+            score +=10
+            scoreDisplay.innerHTML = score
+            row.forEach(index => {
+                squares[index].classList.remove('taken')
+            })
+            const squaresRemoved = squares.splice(i, width)
+        }
+    }
+}
 
 
 
